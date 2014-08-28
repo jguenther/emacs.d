cb19e66bdc0b79d5e06c0d8064f9bb00eed95720
Justin Guenther <jguenther@gmail.com>
2014-08-28 02:12:07 -0600

    Cleaned up init-local.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,16 +1,11 @@
 (setq
-   backup-by-copying t      ; don't clobber symlinks
+   backup-by-copying t               ; don't clobber symlinks
    backup-directory-alist
     '(("." . "~/.emacs.d/saves"))    ; don't litter my fs tree
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
-   version-control t)       ; use versioned backups
-
-
-;; (add-to-list 'package-archives
-;;   '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
+   version-control t)                ; use versioned backups
 
 (setq visible-bell t)
 (setq load-path (cons "/home/jguenther/.emacs-lisp" load-path))
@@ -44,8 +39,6 @@
 
 (require 'bookmark+)
 
-(require 'imenu+)
-
 (require 'doremi)
 (require 'doremi-frm)
 (require 'doremi-cmd)
@@ -61,8 +54,6 @@
 ;(require 'compile-)
 ;(require 'compile+)
 ;(require 'lib-requires)
-;(require 'tool-bar+)
-;
 ;(defun try-to-add-imenu ()
 ;  (condition-case nil (imenu-add-to-menubar "imenu") (error nil)))
 ; (add-hook 'font-lock-mode-hook 'try-to-add-imenu)
@@ -102,12 +93,11 @@
 ;;       (setq hcz-set-cursor-color-buffer (buffer-name)))))
 ;; (add-hook 'post-command-hook 'hcz-set-cursor-color-according-to-mode)
 
-(require 'cursor-chg)  ; Load the library
+(require 'cursor-chg)
 (toggle-cursor-type-when-idle 1) ; Turn on cursor change when Emacs is idle
-(change-cursor-mode 1) ; Turn on change for overwrite, read-only, and input mode
+(change-cursor-mode 1)           ; Turn on change for overwrite, read-only, and input mode
 
 (setq fill-column 79)
-;(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 
 (defadvice load (after give-my-keybindings-priority)
   "Try to ensure that my keybindings always have priority."
@@ -116,25 +106,6 @@
         (assq-delete-all 'my-keys-minor-mode minor-mode-map-alist)
         (add-to-list 'minor-mode-map-alist mykeys))))
 (ad-activate 'load)
-
-;(require 'ebs)
-;(ebs-initialize)
-;(global-set-key [(control tab)] 'ebs-switch-buffer)
-
-;(require 'auto-async-byte-compile)
-;(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
-;(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
-
-;; (defadvice desktop-restore-file-buffer
-;;     (around desktop-read)
-;;   "Be non-interactive while starting a daemon."
-;;   (unless (and (daemonp)
-;;            (server-process))
-;;       (ad-do-it)))
-;; (ad-activate 'desktop-restore-file-buffer)
-
-;(global-set-key (kbd "C-c r") 'desktop-read)
-
 
 (defalias 'perl-mode 'cperl-mode)
 (require 'cperl-mode)
@@ -147,10 +118,6 @@
 
 (add-hook 'cperl-mode-hook 'imenu-add-menubar-index)
 
-;; (add-hook 'cperl-mode-hook
-;;           (lambda () (require 'perl-completion)
-;;             (perl-completion-mode t)))
-
 (add-hook  'cperl-mode-hook
            (lambda ()
              (when (require 'auto-complete nil t)
@@ -160,42 +127,11 @@
                ;;       '(ac-source-perl-completion))
                )))
 
-(add-hook 'cperl-mode-hook 'cperl-mode-my-keys)
-
-(defun cperl-mode-my-keys ()
-  "Set up the keymap for `cperl-mode'."
-
-  (define-key cperl-mode-map [C-return] 'plcmp-cmd-smart-complete)
-  )
-
-;; (autoload 'test-case-mode "test-case-mode" nil t)
-;; (autoload 'enable-test-case-mode-if-test "test-case-mode")
-;; (autoload 'test-case-find-all-tests "test-case-mode" nil t)
-;; (autoload 'test-case-compilation-finish-run-all "test-case-mode")
-
-;; ;To enable it automatically when opening test files:
-;; (add-hook 'find-file-hook 'enable-test-case-mode-if-test)
-
-;; ;If you want to run all visited tests after a compilation, add:
-;; (add-hook 'compilation-finish-functions
-;;           'test-case-compilation-finish-run-all)
-
 (require 'tabbar)
 (tabbar-mode t)
-;(require 'tab-group)
 
-;(setq tabbar-ruler-global-tabbar t)   ; If you want tabbar
-
-;(require 'tabbar-ruler)
 
 (scroll-bar-mode t)
-
-;; ;; el-get stuff -- assume el-get has been installed
-;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-;; (add-to-list 'el-get-user-package-directory "~/.emacs.d/el-get-init")
-
-;; (el-get 'sync)
 
 (require 'git-gutter-fringe+)
 (global-git-gutter+-mode t)
