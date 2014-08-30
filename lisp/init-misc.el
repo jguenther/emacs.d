b26f821d88a16818ffc62e615953e34d06a67ab0
Justin Guenther <jguenther@gmail.com>
2014-08-29 18:52:54 -0600

    Made it so .pm and .t files are never chmod +x'd

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -8,7 +8,22 @@
                   '(prog-mode-hook ruby-mode-hook)
                 '(find-file-hooks)))
   (add-hook hook 'goto-address-prog-mode))
-(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+(defun tak/file-is-pm-or-t ()
+  "Tests whether the current buffer has an associated file and
+   that the file is a .pm file"
+  (and (not (eq nil (buffer-file-name)))
+       (or (string-equal "pm" (file-name-extension (buffer-file-name)))
+           (string-equal "t" (file-name-extension (buffer-file-name))))))
+
+(defun tak/maybe-make-buffer-file-executable ()
+  "Call executable-make-buffer-file-executable-if-script-p unless
+  the file has certain extensions"
+  (if (not (tak/file-is-pm-or-t))
+      (executable-make-buffer-file-executable-if-script-p)))
+
+(add-hook 'after-save-hook 'tak/maybe-make-buffer-file-executable)
+
 (setq goto-address-mail-face 'link)
 
 (setq-default regex-tool-backend 'perl)
