5d1f393297848e9358366d4007afc2efd5fb0303
Justin Guenther <jguenther@gmail.com>
2014-10-30 13:20:18 -0600

    custom.el desktop-mode changes

--- custom.el
+++ custom.el
@@ -20,6 +20,8 @@
    (quote
     ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(desktop-auto-save-timeout 600)
+ '(desktop-lazy-idle-delay 3)
+ '(desktop-restore-eager 3)
  '(desktop-restore-in-current-display t)
  '(desktop-save (quote if-exists))
  '(desktop-save-mode t)
