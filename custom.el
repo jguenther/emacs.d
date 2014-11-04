df18a01441692efa8a6827ad8635fe526e1c1a2a
Justin Guenther <jguenther@gmail.com>
2014-11-04 14:00:12 -0600

    Custom changes -- disabled desktop-mode

--- custom.el
+++ custom.el
@@ -5,10 +5,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#657b83" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#fdf6e3"))
  '(blink-cursor-mode nil)
  '(cperl-close-paren-offset -2)
  '(cperl-comment-column 40)
@@ -18,17 +14,16 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(desktop-auto-save-timeout 600)
  '(desktop-lazy-idle-delay 3)
  '(desktop-restore-eager 3)
  '(desktop-restore-in-current-display t)
  '(desktop-save (quote if-exists))
- '(desktop-save-mode t)
+ '(desktop-save-mode nil)
  '(electric-indent-mode t)
  '(electric-pair-mode nil)
  '(ess-fancy-comments nil)
- '(fci-rule-color "#eee8d5")
  '(fill-column 79)
  '(flycheck-checkers
    (quote
@@ -68,28 +63,6 @@
  '(tabbar-scroll-left-button (quote ((" <") " =")))
  '(tabbar-scroll-right-button (quote ((" >") " =")))
  '(tabbar-separator (quote ("|")))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#cb4b16")
-     (60 . "#b58900")
-     (80 . "#859900")
-     (100 . "#2aa198")
-     (120 . "#268bd2")
-     (140 . "#d33682")
-     (160 . "#6c71c4")
-     (180 . "#dc322f")
-     (200 . "#cb4b16")
-     (220 . "#b58900")
-     (240 . "#859900")
-     (260 . "#2aa198")
-     (280 . "#268bd2")
-     (300 . "#d33682")
-     (320 . "#6c71c4")
-     (340 . "#dc322f")
-     (360 . "#cb4b16"))))
- '(vc-annotate-very-old-color nil)
  '(win-switch-idle-time 1.5))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
