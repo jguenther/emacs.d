86cce288b913ac88e0c8de94d4c954b80c11fd5c
Justin Guenther <jguenther@gmail.com>
2014-11-04 14:03:21 -0600

    Fill line

--- init.el
+++ init.el
@@ -7,7 +7,8 @@
   (when (version<= emacs-version "23.1")
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
 (when (version<= emacs-version "24")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+  (message "Your Emacs is old, and some functionality in this
+  config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
