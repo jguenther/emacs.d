5bea3319a57c3227d1712c5f1ed3c6ded65432e4
Justin Guenther <jguenther@gmail.com>
2014-11-15 00:55:48 -0600

    Fix tak/maybe-make-buffer-file-executable and related defuns and defcustom

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -9,20 +9,30 @@
                 '(find-file-hooks)))
   (add-hook hook 'goto-address-prog-mode))
 
-(defun tak/file-is-pm-or-t ()
-  "Tests whether the current buffer has an associated file and
-   that the file is a .pm file"
-  (and (not (eq nil (buffer-file-name)))
-       (or (string-equal "pm" (file-name-extension (buffer-file-name)))
-           (string-equal "t" (file-name-extension (buffer-file-name))))))
+(defcustom non-exec-script-file-extensions '("pm" "t")
+  "A list of script file extensions that should not be made executable.
+These are used by `tak/buffer-file-ext-is-noexec-p'.
+See also: `tak/maybe-make-buffer-file-executable-if-script-p'."
+  :type '(repeat string)
+  :options '("pm" "t")
+  :group 'files)
+
+(defun tak/buffer-file-ext-is-exec-p ()
+  "Tests whether the current buffer has an associated file and that
+   the file's extension isn't in `non-exec-script-file-extensions'."
+  (let* ((file buffer-file-name)
+         (ext (file-name-extension file)))
+    (and file ext
+         (not (member ext non-exec-script-file-extensions)))))
 
 (defun tak/maybe-make-buffer-file-executable ()
-  "Call executable-make-buffer-file-executable-if-script-p unless
-  the file has certain extensions"
-  (if (not (tak/file-is-pm-or-t))
+  "Call executable-make-buffer-file-executable-if-script-p if
+   tak/buffer-file-ext-is-noexec-p returns `nil'."
+  (if (tak/buffer-file-ext-is-exec-p)
       (executable-make-buffer-file-executable-if-script-p)))
 
-;(add-hook 'after-save-hook 'tak/maybe-make-buffer-file-executable)
+(add-hook 'after-save-hook
+          'tak/maybe-make-buffer-file-executable t nil)
 
 (setq goto-address-mail-face 'link)
 
