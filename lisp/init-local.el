8c35c214215ef15ac167cf1778ea8f6dc095765c
Justin Guenther <jguenther@gmail.com>
2014-11-21 23:45:29 -0600

    Moved tak/maybe-make-buffer-file-executable-if-script-p and related to init-misc.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -191,31 +191,6 @@
 (ess-toggle-underscore nil)
 (ess-default-style (quote OWN))
 
-(defcustom non-exec-script-file-extensions '("pm" "t")
-  "A list of script file extensions that should not be made executable.
-These are used by `tak/buffer-file-ext-is-noexec-p'.  See also:
-`tak/maybe-make-buffer-file-executable-if-script-p'."
-  :type '(repeat string)
-  :options '("pm" "t")
-  :group 'files)
-
-(defun tak/buffer-file-ext-is-exec-p ()
-  "Tests whether the current buffer has an associated file
-and that the file's extension isn't in `non-exec-script-file-extensions'."
-  (let* ((file buffer-file-name)
-         (ext (file-name-extension file)))
-    (and file ext
-         (not (member ext non-exec-script-file-extensions)))))
-
-(defun tak/maybe-make-buffer-file-executable ()
-  "Call executable-make-buffer-file-executable-if-script-p
-if tak/buffer-file-ext-is-noexec-p returns `nil'."
-  (if (tak/buffer-file-ext-is-exec-p)
-      (executable-make-buffer-file-executable-if-script-p)))
-
-(add-hook 'after-save-hook
-          'tak/maybe-make-buffer-file-executable t nil)
-
 ;(global-aggressive-indent-mode t)
 
 (provide 'init-local)
