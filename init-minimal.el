d78d046f936c78809695c850af6f55a73dff9853
Justin Guenther <jguenther@gmail.com>
2014-12-14 14:52:53 -0600

    added minimal init file for debugging with `emacs -Q`

---
+++ init-minimal.el
@@ -0,0 +1,20 @@
+;; -*-no-byte-compile: t; -*-
+;;
+;;; This file loads a minimal configuration for init debugging purposes.
+
+(let ((minver "23.3"))
+  (when (version<= emacs-version "23.1")
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version<= emacs-version "24")
+  (message "Your Emacs is old, and some functionality in this
+  config will be disabled. Please upgrade if possible."))
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+(require 'init-compat)
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
