c048a646bd3bef15423331e378ea86c681ea7802
Justin Guenther <jguenther@gmail.com>
2015-01-08 17:51:22 -0600

    Remove unused my-keys-minor-mode-map

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -15,33 +15,6 @@
   (dolist (path load-paths)
     (add-to-list 'load-path path)))
 
-;;
-;; override keybindings with new minor mode
-;;
-(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
-
-(define-minor-mode my-keys-minor-mode
-  "A minor mode so that my key settings override annoying major modes."
-  t "" 'my-keys-minor-mode-map)
-
-;; custom keybindings
-;; no longer needed -- C-I is not bound, can't remember what was binding this
-;(define-key my-keys-minor-mode-map (kbd "C-I") 'imenu)
-;(my-keys-minor-mode 1)
-
-;; make sure my-keys-minor-mode doesn't mess with minibuffer
-(defun my-minibuffer-setup-hook ()
-  (my-keys-minor-mode 0))
-(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
-
-(defadvice load (after give-my-keybindings-priority)
-  "Try to ensure that my keybindings always have priority."
-  (if (not (eq (car (car minor-mode-map-alist)) 'my-keys-minor-mode))
-      (let ((mykeys (assq 'my-keys-minor-mode minor-mode-map-alist)))
-        (assq-delete-all 'my-keys-minor-mode minor-mode-map-alist)
-        (add-to-list 'minor-mode-map-alist mykeys))))
-(ad-activate 'load)
-
 (global-set-key "\M-g" 'goto-line)
 (mouse-wheel-mode 1)
 
