f3902fb0c8b7ecb39b2fab28d272ef03b19ed0c5
Justin Guenther <jguenther@gmail.com>
2015-01-14 16:30:00 -0600

    Bind `help-mode` keys in `help-mode-hook`

--- lisp/init-help.el
+++ lisp/init-help.el
@@ -1,13 +1,9 @@
 (require 'help-mode)
 
-(add-hook 'ido-setup-hook 'ido-my-keys)
-
-(defun ido-my-keys ()
-  "Set up the keymap for `ido'."
-
-  (define-key help-mode-map [(meta left)] 'help-go-back)
-  (define-key help-mode-map [(meta right)] 'help-go-forward)
-  )
+(add-hook 'help-mode-hook
+          (lambda ()
+            (define-key help-mode-map [(meta left)] 'help-go-back)
+            (define-key help-mode-map [(meta right)] 'help-go-forward)))
 
 ;;; describe this point lisp only
 ;;
