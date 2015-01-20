73b1107944a4415707e3f3969c59ef5797f033a3
Justin Guenther <jguenther@gmail.com>
2015-01-20 17:47:50 -0600

    init extra dired packages after it's loaded

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,5 +1,9 @@
 (require-package 'dired+)
 (require-package 'dired-sort)
+(require-package 'dired-hacks-utils)
+(require-package 'dired-filter)
+(require-package 'dired-imenu)
+(require-package 'dired-sort-menu)
 
 (setq-default diredp-hide-details-initially-flag nil
               dired-dwim-target t)
@@ -7,16 +11,15 @@
 (after-load 'dired
   (require 'dired+)
   (require 'dired-sort)
+  (require 'dired-hacks-utils)
+  (require 'dired-filter)
+  (require 'dired-imenu)
+  (require 'dired-sort-menu)
   (when (fboundp 'global-dired-hide-details-mode)
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
   (define-key dired-mode-map [mouse-2] 'dired-find-file)
   (add-hook 'dired-mode-hook
             (lambda () (guide-key/add-local-guide-key-sequence "%"))))
-
-(require 'dired-hacks-utils)
-(require 'dired-filter)
-(require 'dired-imenu)
-(require 'dired-sort-menu)
 
 (provide 'init-dired)
