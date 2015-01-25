e74006195278b54fecc648e998dce69392511042
Justin Guenther <jguenther@gmail.com>
2015-01-25 13:12:33 -0600

    Fix `tak/buffer-file-ext-is-noexec-p' for files without extensions

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -24,12 +24,15 @@ These are used by `tak/buffer-file-ext-is-noexec-p'.  See also:
   :group 'files)
 
 (defun tak/buffer-file-ext-is-exec-p ()
-  "Tests whether the current buffer has an associated file
-and that the file's extension isn't in `non-exec-script-file-extensions'."
+  "Returns non-nil if the buffer has a file and the file either
+doesn't have an extension or the extension isn't in
+`non-exec-script-file-extensions'. Returns nil otherwise."
   (let* ((file buffer-file-name)
-         (ext (file-name-extension file)))
-    (and file ext
-         (not (member ext non-exec-script-file-extensions)))))
+         (ext (if file (file-name-extension file))))
+    (if file
+        (if ext
+            (not (member ext non-exec-script-file-extensions))
+          t))))
 
 (defun tak/maybe-make-buffer-file-executable ()
   "Call executable-make-buffer-file-executable-if-script-p
