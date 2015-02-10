f70e8489a84c59bd008814afaffd9cb7d4081d68
Justin Guenther <jguenther@gmail.com>
2015-02-10 13:36:36 -0600

    Ignore ELPA and git message-related files in file-name-history

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -79,5 +79,10 @@
     (require-package 'frame-restore)
     (frame-restore)))
 
+(setq-default session-set-file-name-exclude-regexp
+              (concat session-set-file-name-exclude-regexp
+                      "\\|\\.emacs\\.d/elpa/"
+                      "\\|/\\.git/\\(COMMIT\\|MERGE\\)_\\(EDIT\\)?MSG"))
+
 
 (provide 'init-sessions)
