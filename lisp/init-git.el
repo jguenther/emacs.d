269c8afac7c4a77f52d7832e600e3bb0affb9304
Justin Guenther <jguenther@gmail.com>
2015-03-12 13:40:10 -0600

    Add Github PR hook

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -114,6 +114,23 @@
                 (append magit-git-standard-options (list "-c" "color.ui=true"))))
 
 
+
+
+;; from http://endlessparentheses.com/automatically-configure-magit-to-access-github-prs.html
+(defun endless/add-PR-fetch ()
+  "If refs/pull is not defined on a GH repo, define it."
+  (let ((fetch-address
+         "+refs/pull/*/head:refs/pull/origin/*"))
+    (unless (member
+             fetch-address
+             (magit-get-all "remote" "origin" "fetch"))
+      (when (string-match
+             "github\\.com" (magit-get "remote" "origin" "url"))
+        (magit-git-string
+         "config" "--add" "remote.origin.fetch"
+         fetch-address)))))
+
+(add-hook 'magit-mode-hook #'endless/add-PR-fetch)
 
 
 
