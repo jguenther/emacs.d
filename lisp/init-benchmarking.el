774c7b5e1e2b3dec8c2ede1c4c9cdf18d029697a
Justin Guenther <jguenther@gmail.com>
2015-07-22 11:40:41 -0600

    Init benchmarking

--- lisp/init-benchmarking.el
+++ lisp/init-benchmarking.el
@@ -23,14 +23,26 @@ LOAD-DURATION is the time taken in milliseconds to load FEATURE.")
 (defun tak/print-require-times ()
   "Prints the require times stored in `sanityinc/require-times'."
   (interactive)
-  (let ((list sanityinc/require-times))
+  (let ((list sanityinc/require-times)
+        sorted-times)
     (while list
       (let ((package-time (car list)))
         (setq list (cdr list))
         (let ((package (car package-time))
               (time (cdr package-time)))
+          (add-to-ordered-list 'sorted-times (list package time) time))))
+    (setq sorted-times (reverse sorted-times))
+    (setq tak/sorted-times sorted-times)
+    ;;(setq sanityinc/require-times sorted-times)
+    (while sorted-times
+      (let ((package-time (car sorted-times)))
+        (setq sorted-times (cdr sorted-times))
+        (let ((package (car package-time))
+              (time (car (cdr package-time))))
           (message "feature `%s': %d ms." package time))))))
 
-
+(add-to-list 'load-path "~/code/benchmark-init-el")
+(require 'benchmark-init-loaddefs)
+(benchmark-init/activate)
 
 (provide 'init-benchmarking)
