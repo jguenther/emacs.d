d09dbce76e49904a2934b95a8bb170770ae81217
Justin Guenther <jguenther@gmail.com>
2015-08-13 12:22:18 -0600

    Initial helm config (not enabled yet)

---
+++ lisp/init-helm.el
@@ -0,0 +1,112 @@
+(require-package 'helm)
+
+(require-package 'helm-bind-key)
+(require-package 'helm-descbinds)
+(require-package 'helm-dired-recent)
+;;(require-package 'helm-flycheck)
+(require-package 'helm-fuzzy-find)
+;;(require-package 'helm-git)
+(require-package 'helm-git-grep)
+(require-package 'helm-ls-git)
+;;(require-package 'helm-make)
+(require-package 'helm-orgcard)
+(require-package 'helm-projectile)
+(require-package 'helm-pydoc)
+(require-package 'wgrep-helm)
+(require-package 'helm-ag)
+
+;;; http://emacs.stackexchange.com/questions/2867/how-should-i-change-my-workflow-when-moving-from-ido-to-helm
+
+(setq-default helm-ff-transformer-show-only-basename nil
+              helm-adaptive-history-file             "~/.emacs.d/data/helm-history"
+              helm-yank-symbol-first                 t
+              helm-move-to-line-cycle-in-source      t
+              helm-buffers-fuzzy-matching            t
+              helm-ff-auto-update-initial-value      t)
+
+(autoload 'helm-descbinds      "helm-descbinds" t)
+(autoload 'helm-eshell-history "helm-eshell"    t)
+(autoload 'helm-esh-pcomplete  "helm-eshell"    t)
+
+(global-set-key (kbd "C-h a")    #'helm-apropos)
+(global-set-key (kbd "C-h i")    #'helm-info-emacs)
+(global-set-key (kbd "C-h b")    #'helm-descbinds)
+
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map (kbd "TAB")     #'helm-esh-pcomplete)
+              (define-key eshell-mode-map (kbd "C-c C-l") #'helm-eshell-history)))
+
+(global-set-key (kbd "C-x b")   #'helm-mini)
+(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
+(global-set-key (kbd "C-x C-m") #'helm-M-x)
+(global-set-key (kbd "M-x")     #'helm-M-x)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-x C-r") #'helm-recentf)
+(global-set-key (kbd "C-x r l") #'helm-filtered-bookmarks)
+(global-set-key (kbd "M-y")     #'helm-show-kill-ring)
+(global-set-key (kbd "M-s o")   #'helm-swoop)
+(global-set-key (kbd "M-s /")   #'helm-multi-swoop)
+
+(require 'helm-config)
+(helm-mode t)
+(helm-adaptative-mode t)
+
+(global-set-key (kbd "C-x c!")   #'helm-calcul-expression)
+(global-set-key (kbd "C-x c:")   #'helm-eval-expression-with-eldoc)
+(define-key helm-map (kbd "M-o") #'helm-previous-source)
+
+(global-set-key (kbd "M-s s")   #'helm-ag)
+
+(require 'helm-projectile)
+(setq helm-projectile-sources-list (cons 'helm-source-projectile-files-list
+                                         (remove 'helm-source-projectile-files-list
+                                                 helm-projectile-sources-list)))
+(helm-projectile-on)
+
+(define-key projectile-mode-map (kbd "C-c p /")
+  #'(lambda ()
+      (interactive)
+      (helm-ag (projectile-project-root))))
+
+(define-key org-mode-map (kbd "C-x c o h") #'helm-org-headlines)
+
+;;; https://github.com/thierryvolpiatto/emacs-tv-config/blob/master/init-helm-thierry.el
+;;;
+;;; Helm-command-map
+;;
+;;
+(define-key helm-command-map (kbd "w")   'helm-psession)
+(define-key helm-command-map (kbd "z")   'helm-complex-command-history)
+(define-key helm-command-map (kbd "I")   'helm-imenu-in-all-buffers)
+
+;;; Global-map
+;;
+;;
+(global-set-key (kbd "M-x")                          'undefined)
+(global-set-key (kbd "M-x")                          'helm-M-x)
+(global-set-key (kbd "M-y")                          'helm-show-kill-ring)
+(global-set-key (kbd "C-c f")                        'helm-recentf)
+(global-set-key (kbd "C-x C-f")                      'helm-find-files)
+(global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
+(global-set-key (kbd "C-x r b")                      'helm-filtered-bookmarks)
+(global-set-key (kbd "C-h r")                        'helm-info-emacs)
+(global-set-key (kbd "C-:")                          'helm-eval-expression-with-eldoc)
+(global-set-key (kbd "C-,")                          'helm-calcul-expression)
+(global-set-key (kbd "C-h i")                        'helm-info-at-point)
+(global-set-key (kbd "C-x C-d")                      'helm-browse-project)
+(global-set-key (kbd "<f1>")                         'helm-resume)
+(global-set-key (kbd "C-h C-f")                      'helm-apropos)
+(global-set-key (kbd "<f5> s")                       'helm-find)
+(global-set-key (kbd "<f2>")                         'helm-execute-kmacro)
+(global-set-key (kbd "C-c g")                        'helm-gid)
+(global-set-key (kbd "C-c i")                        'helm-imenu-in-all-buffers)
+(define-key global-map [remap jump-to-register]      'helm-register)
+(define-key global-map [remap list-buffers]          'helm-buffers-list)
+(define-key global-map [remap dabbrev-expand]        'helm-dabbrev)
+(define-key global-map [remap find-tag]              'helm-etags-select)
+(define-key global-map [remap xref-find-definitions] 'helm-etags-select)
+(define-key shell-mode-map (kbd "M-p")               'helm-comint-input-ring) ; shell history.
+
+
+(provide 'init-helm)
