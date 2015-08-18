d6fb1e9ab55a2ba6fad61381a768d71f6ccbadf1
Justin Guenther <jguenther@gmail.com>
2015-08-18 11:21:56 -0600

    Remove ant config

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -98,28 +98,5 @@
       (ansi-color-apply-on-region compilation-filter-start (point-max))))
   (add-hook 'compilation-filter-hook 'sanityinc/colourise-compilation-buffer))
 
-(defvar ant-command-history nil
-  "Ant command history variable")
-
-(defun ant-compile(&optional args)
-  "Runs ant in the current project. Starting at the directory
-   where the file being visited resides, a search is made for
-   build.xml recursively. A maven command is made from the first
-   directory where the build.xml file is found is then displayed in
-   the minibuffer. The command can be edited as needed and then
-   executed. Errors are navigate to as in any other compile mode"
-  (interactive)
-  (let ((fn (buffer-file-name)))
-    (let ((dir (file-name-directory fn)))
-      (while (and (not (file-exists-p (concat dir "/build.xml")))
-                  (not (equal dir (file-truename (concat dir "/..")))))
-        (setf dir (file-truename (concat dir "/.."))))
-      (if (not (file-exists-p (concat dir "/build.xml")))
-          (message "No build.xml found")
-        (compile (read-from-minibuffer "Command: "
-                                       (concat "ant -emacs -f "
-                                               dir "/build.xml compile") nil
-                                               nil
-                                               'ant-command-history))))))
 
 (provide 'init-compile)
