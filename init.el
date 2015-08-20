3de579dce8ff9ae6cf9c89c36e02c9b98279d199
Justin Guenther <jguenther@gmail.com>
2015-08-20 08:20:26 -0600

    Load custom later

--- init.el
+++ init.el
@@ -18,7 +18,6 @@
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
 
 (require 'init-compat)
 (require 'init-utils)
@@ -26,6 +25,8 @@
 ;; Calls (package-initialize)
 (require 'init-elpa)      ;; Machinery for installing required packages
 (require 'init-exec-path) ;; Set up $PATH
+
+(load custom-file)
 
 ;;----------------------------------------------------------------------------
 ;; Allow users to provide an optional "init-preload-local.el"
