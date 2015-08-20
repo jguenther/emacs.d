8542cfcbcb1c9ebe77c243252bc54dbb28705751
Justin Guenther <jguenther@gmail.com>
2015-08-20 12:47:06 -0600

    python init reorg

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -2,16 +2,24 @@
                                         ("SConscript\\'" . python-mode))
                                       auto-mode-alist)
               elpy-rpc-backend "jedi"
-              indent-guide-recursive t
+              indent-guide-recursive nil
               jedi:setup-keys t
-              jedi:complete-on-dot t)
+              jedi:complete-on-dot t
+              jedi-direx:hide-imports t)
 
 (require-package 'pip-requirements)
 (add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup)
 
 (require-package 'elpy)
-
 (require-package 'yasnippet)
+
+(require-package 'indent-guide)
+(require-package 'python-x)
+
+
+
+;;;
+;;; elpy setup
 
 (after-load 'elpy
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
@@ -19,75 +27,102 @@
   
   ;; use jedi completion instead
   (setq elpy-modules (delq 'elpy-module-company elpy-modules))
+
   (unless (require 'yasnippet nil t)
     (setq elpy-modules (delq 'elpy-module-yasnippet elpy-modules)))
 
+  ;; remove unnecessary binds
+  (cl-dolist (key '(
                                         ; flymake
-  (cl-dolist (key '("n" "p" "C-n" "C-p"
+                    "C-c n" "C-c p" "C-c C-n" "C-c C-p"
                                         ; refactor (deprecated)
-                    "C-r"
+                    "C-c C-r"
                                         ; ??
-                    "R"
+                    "C-c R"
+                                        ; elpy-check
+                    "C-c C-v"
                     ))
-    (define-key elpy-mode-map (kbd key) nil))
-  
+    (define-key elpy-mode-map (kbd key) nil)))
+
+(after-load 'python
   ;; Skeletons
-  (define-key python-mode-map "\C-csc" 'python-skeleton-class)
-  (define-key python-mode-map "\C-csd" 'python-skeleton-def)
-  (define-key python-mode-map "\C-csf" 'python-skeleton-for)
-  (define-key python-mode-map "\C-csi" 'python-skeleton-if)
-  (define-key python-mode-map "\C-cst" 'python-skeleton-try)
-  (define-key python-mode-map "\C-csw" 'python-skeleton-while))
+  (define-key python-mode-map (kbd "C-c s c") 'python-skeleton-class)
+  (define-key python-mode-map (kbd "C-c s d") 'python-skeleton-def)
+  (define-key python-mode-map (kbd "C-c s f") 'python-skeleton-for)
+  (define-key python-mode-map (kbd "C-c s i") 'python-skeleton-if)
+  (define-key python-mode-map (kbd "C-c s t") 'python-skeleton-try)
+  (define-key python-mode-map (kbd "C-c s w") 'python-skeleton-while)
 
-(require-package 'indent-guide)
-(require 'indent-guide)
+  (add-hook 'python-mode-hook 'superword-mode)
+  (add-hook 'python-mode-hook 'indent-guide-mode)
+  (add-hook 'magit-blame-mode-hook (lambda () (when indent-guide-mode
+                                           (indent-guide-mode))))
 
-;;(require-package 'flycheck-pyflakes)
+  ;; enable this mode in tak/python-setup instead
+  (add-hook 'python-mode-hook (lambda () (flycheck-mode -1)))
 
-(require-package 'jedi)
+  
 
+  (add-hook 'comint-output-filter-functions 'python-pdbtrack-comint-output-filter-function)
+
+  ;; python-x binds
+  (define-key python-mode-map (kbd "C-c ! C-j") 'python-shell-send-line)
+  (define-key python-mode-map (kbd "C-c ! C-n") 'python-shell-send-line-and-step)
+  (define-key python-mode-map (kbd "C-c ! C-f") 'python-shell-send-defun)
+  (define-key python-mode-map (kbd "C-c ! C-b") 'python-shell-send-buffer)
+  (define-key python-mode-map (kbd "C-c ! C-c") 'python-shell-send-dwim)
+  (define-key python-mode-map (kbd "C-c ! p") 'python-shell-print-region-or-symbol)
+  )
+
+
+
+;;; jedi config
 
 ;; run `(jedi:install-server)' manually after installation and after each
 ;; update to jedi
 
-(require-package 'pydoc)
+(require-package 'jedi)
+(require-package 'jedi-direx)
 
-;;(require-package 'pytest)
+(after-load 'jedi
+  (define-key python-mode-map (kbd "C-c J t") 'jedi:toggle-log-traceback)
+  (define-key python-mode-map (kbd "C-c J d") 'jedi:toggle-debug-server)
+  (define-key python-mode-map (kbd "C-c J e") 'jedi:pop-to-epc-buffer)
 
-(require-package 'python-x)
-(eval-after-load 'python
-  (lambda ()
-    (require 'python-x)    
-    (define-key python-mode-map (kbd "C-c ! C-j") 'python-shell-send-line)
-    (define-key python-mode-map (kbd "C-c ! C-n") 'python-shell-send-line-and-step)
-    (define-key python-mode-map (kbd "C-c ! C-f") 'python-shell-send-defun)
-    (define-key python-mode-map (kbd "C-c ! C-b") 'python-shell-send-buffer)
-    (define-key python-mode-map (kbd "C-c ! C-c") 'python-shell-send-dwim)
-    (define-key python-mode-map (kbd "C-c ! p") 'python-shell-print-region-or-symbol)))
+  (define-key python-mode-map (kbd "C-c C-.") 'jedi:goto-definition-pop-marker)
+  (define-key python-mode-map (kbd "C-.")     'jedi:goto-definition-push-marker)
+  (define-key python-mode-map (kbd "C-c C-.")   'jedi:goto-definition-push-marker)
 
-;;; pungi (jedi and virtualenv compat)
+  (add-hook 'jedi-mode-hook 'jedi-direx:setup)
+  (define-key python-mode-map (kbd "C-c x") 'jedi-direx:pop-to-buffer)
+  )
+
+;; pungi (jedi and virtualenv compat)
 ;;(require-package 'pungi)
 ;;(add-hook 'python-mode-hook (lambda () (pungi:setup-jedi)))
 
-(require-package 'python-docstring)
-;;(require-package 'python-info)
-
-(require-package 'pydoc-info)
-(after-load 'python-mode
-  (require 'pydoc-info))
-
 ;;(require-package 'company-jedi)
 
-;;pdb setup, note the python version
-(setq pdb-path '/usr/lib/python2.7/pdb.py
-      gud-pdb-command-name (symbol-name pdb-path))
+
 
-(defadvice pdb (before gud-query-cmdline activate)
-  "Provide a better default command line when called interactively."
-  (interactive
-   (list (gud-query-cmdline pdb-path
-	 		    (file-name-nondirectory buffer-file-name)))))
+;;;
+;;; documentation setup
 
+(require-package 'pydoc)
+(require-package 'python-docstring)
+;;(require-package 'python-info)
+(require-package 'pydoc-info)
+
+;; (after-load 'python-mode
+;;   (require 'pydoc-info))
+
+
+
+;;;
+;;; ipython setup
+
+;; out of date?
+;;
 ;; (defvar server-buffer-clients)
 ;; (when (and (fboundp 'server-start) (string-equal (getenv "TERM") 'xterm))
 ;;   (server-start)
@@ -107,85 +142,21 @@
 ;;       python-shell-completion-string-code
 ;;       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
+
+
+;;; testing setup
+
+;;; emacs-dbgr / realgud
 
 ;; realgud: use pdb, trepan2, or pydbgr
 
-(require-package 'jedi-direx)
-(after-load 'python
-  (define-key python-mode-map "\C-cx" 'jedi-direx:pop-to-buffer)
-  ;;(require 'realgud)
-  ;;(require 'pytest)
-  )
-(add-hook 'jedi-mode-hook 'jedi-direx:setup)
-
-(add-hook 'comint-output-filter-functions 'python-pdbtrack-comint-output-filter-function)
-
-(defun annotate-pdb-breakpoints ()
-  (interactive)
-  (highlight-lines-matching-regexp "import i?pdb")
-  (highlight-lines-matching-regexp "i?pdb.set_trace()"))
-
-
-(defun python-add-breakpoint ()
-  (interactive)
-  (beginning-of-line)
-  (newline-and-indent)
-  (previous-line)
-  (beginning-of-line)
-  ;;  (insert "import ipdb; ipdb.set_trace()")
-  (insert "import pdb; pdb.set_trace()")
-  (indent-according-to-mode)
-  )
-
-(define-key python-mode-map (kbd "C-c <SPC>") 'python-add-breakpoint)
-;;(define-key python-mode-map (kbd "C-c C-<SPC>") 'python-add-breakpoint)
-
-(add-hook 'python-mode-hook 'indent-guide-mode)
-(add-hook 'magit-blame-mode-hook (lambda () (when indent-guide-mode
-                                    (indent-guide-mode))))
-
-(add-hook 'python-mode-hook 'annotate-pdb-breakpoints)
-
-;; enable this mode in tak/python-setup instead
-(add-hook 'python-mode-hook (lambda () (flycheck-mode -1)))
-
-(add-hook 'python-mode-hook 'superword-mode)
-
-;;; emacs-dbgr / realgud
 ;;(require-package 'test-simple)
 ;;(require-package 'load-relative)
 ;;(require-package 'loc-changes)
 ;;(require-package 'realgud)
 
-
-;; Use the regular major mode hook to add a buffer-local hack-local-variables-hook
-;;
-;; This is necessary since python sys.path is set in dirlocals which is not
-;; visible until after python-mode-hook has run
-;;
-(defun tak/python-setup ()
-  ;;(message "in tak/python-setup")
-  (add-hook
-   'hack-local-variables-hook
-   (lambda ()
-     ;;(message "in tak/python-setup lambda")
-     (add-hook 'python-mode-hook 'jedi:setup)
-     ;;(add-hook 'python-mode-hook 'jedi:start-dedicated-server)
-     (add-hook 'python-mode-hook 'flycheck-mode)
-     
-     ;; (setq elpy-rpc-pythonpath (mapconcat 'concat '(elpy-rpc-pythonpath
-     ;;                                                python-shell-extra-pythonpaths)))
-     
-     ) nil t))
-
-;; ;; alternate method
-;; (add-hook 'hack-local-variables-hook 'run-local-vars-mode-hook)
-;; (defun run-local-vars-mode-hook ()
-;;   "Run a hook for the major-mode after the local variables have been processed."
-;;   (run-hooks (intern (concat (symbol-name major-mode) "-local-vars-hook"))))
-;; (add-hook 'python-mode-local-vars-hook 'cr/python-mode-shell-setup)
-
-(add-hook 'python-mode-hook 'tak/python-setup)
+;; disabled--elpy appears to provide all functionality
+;;(require-package 'pytest)
 
 ;; TODO make this append to PYTHONPATH instead of replacing it
 (defadvice elpy-test (around manipulate-environment activate)
@@ -204,6 +175,35 @@ environment variable."
     (setenv "TERM" term)
     ))
 
+;;
+;; pdb setup, note the python version
+(setq pdb-path '/usr/lib/python2.7/pdb.py
+      gud-pdb-command-name (symbol-name pdb-path))
+
+(defadvice pdb (before gud-query-cmdline activate)
+  "Provide a better default command line when called interactively."
+  (interactive
+   (list (gud-query-cmdline pdb-path
+	 		    (file-name-nondirectory buffer-file-name)))))
+
+(defun annotate-pdb-breakpoints ()
+  (interactive)
+  (highlight-lines-matching-regexp "import i?pdb")
+  (highlight-lines-matching-regexp "i?pdb.set_trace()"))
+
+(defun python-add-breakpoint ()
+  (interactive)
+  (beginning-of-line)
+  (newline-and-indent)
+  (previous-line)
+  (beginning-of-line)
+  ;;  (insert "import ipdb; ipdb.set_trace()")
+  (insert "import pdb; pdb.set_trace()")
+  (indent-according-to-mode)
+  )
+
+(define-key python-mode-map (kbd "C-c <SPC>") 'python-add-breakpoint)
+(add-hook 'python-mode-hook 'annotate-pdb-breakpoints)
 
 ;; (let ((pythonpath "PYTHONPATH")
 ;;       (pythonpaths '("a" "b")))
@@ -216,7 +216,6 @@ environment variable."
 ;;                                nil))
 ;;            )))
 
-(require 'pytest-emacs)
 
 
 ;; from https://gist.github.com/TauPan/17305751a883005872dc
@@ -280,7 +279,37 @@ environment variable."
 ;;; for virtualenv setup with jedi (out of date?):
 ;;; http://stackoverflow.com/questions/21246218/how-can-i-make-emacs-jedi-use-project-specific-virtualenvs
 
-(elpy-enable)
+
 
+;;; deferred setup, to deal with pythonpath issues
+
+;;
+;; Use the regular major mode hook to add a buffer-local hack-local-variables-hook
+;;
+;; This is necessary since python sys.path is set in dirlocals which is not
+;; visible until after python-mode-hook has run
+;;
+(defun tak/python-setup ()
+  (add-hook
+   'hack-local-variables-hook
+   (lambda ()
+     (add-hook 'python-mode-hook 'jedi:setup)
+     (add-hook 'python-mode-hook 'flycheck-mode)
+     
+     ;; (setq elpy-rpc-pythonpath (mapconcat 'concat '(elpy-rpc-pythonpath
+     ;;                                                python-shell-extra-pythonpaths)))
+     
+     ) nil t))
+
+;; ;; alternate method
+;; (add-hook 'hack-local-variables-hook 'run-local-vars-mode-hook)
+;; (defun run-local-vars-mode-hook ()
+;;   "Run a hook for the major-mode after the local variables have been processed."
+;;   (run-hooks (intern (concat (symbol-name major-mode) "-local-vars-hook"))))
+;; (add-hook 'python-mode-local-vars-hook 'cr/python-mode-shell-setup)
+
+(add-hook 'python-mode-hook 'tak/python-setup)
+
+(elpy-enable)
 
 (provide 'init-python-mode)
