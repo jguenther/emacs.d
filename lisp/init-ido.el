ac18ef4b0ffc342fb3b32335c4c90a4531597b01
Justin Guenther <jguenther@gmail.com>
2015-09-03 13:51:26 -0600

    ido init

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -22,7 +22,7 @@
 (ido-mode t)
 (ido-everywhere t)
 (setq ido-enable-flex-matching t)
-(setq ido-use-filename-at-point nil)
+(setq ido-use-filename-at-point t)
 (setq ido-auto-merge-work-directories-length 0)
 (setq ido-use-virtual-buffers t)
 
@@ -40,8 +40,9 @@
 (require-package 'idomenu)
 (autoload 'idomenu "idomenu" nil t)
 
-;; Allow the same buffer to be open in different frames
+;; Allow the same buffer/file to be open in different frames
 (setq ido-default-buffer-method 'selected-window)
+(setq ido-default-file-method 'selected-window)
 
 ;; http://www.reddit.com/r/emacs/comments/21a4p9/use_recentf_and_ido_together/cgbprem
 (add-hook 'ido-setup-hook (lambda () (define-key ido-completion-map [up] 'previous-history-element)))
