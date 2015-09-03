79c0c1db78f177a50d518b8c45f97f6908563786
Justin Guenther <jguenther@gmail.com>
2015-09-03 14:22:49 -0600

    Don't use diff-hl at all

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,13 +1,13 @@
-(require-package 'diff-hl)
+;; (require-package 'diff-hl)
 
 ;; (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
 ;; (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
 
-(defun tak/add-diff-hl-toggle-command ()
-  (define-key tak/vc-toggle-map "d" 'diff-hl-mode))
+;; (defun tak/add-diff-hl-toggle-command ()
+;;   (define-key tak/vc-toggle-map "d" 'diff-hl-mode))
 
-(add-hook 'prog-mode-hook 'tak/add-diff-hl-toggle-command)
-(add-hook 'vc-dir-mode-hook 'tak/add-diff-hl-toggle-command)
+;; (add-hook 'prog-mode-hook 'tak/add-diff-hl-toggle-command)
+;; (add-hook 'vc-dir-mode-hook 'tak/add-diff-hl-toggle-command)
 
 
 (provide 'init-vc)
