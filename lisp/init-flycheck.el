946fa13867823d6fa3cff5e72894b90c0b8245d7
Justin Guenther <jguenther@gmail.com>
2015-09-09 10:02:28 -0600

    Use dir under temporary-file-directory for flycheck temp files

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -5,7 +5,10 @@
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
         flycheck-idle-change-delay 0.8)
 
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+
+  (setq-default flycheck-temp-prefix (expand-file-name "flycheck/_flycheck_" temporary-file-directory))
+  )
 
 
 (provide 'init-flycheck)
