b008916d4cd04dc2f0d454dae49296b9f5b90019
Justin Guenther <jguenther@gmail.com>
2015-10-06 10:48:08 -0600

    Remove comment

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -128,14 +128,6 @@ running."
       python-shell-interpreter-args "-i"
       python-shell-prompt-regexp "In \\[[0-9]+\\]: "
       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-      
-      ;; python-shell-completion-setup-code
-      ;; "from IPython.core.completerlib import module_completion"
-      ;; python-shell-completion-module-string-code
-      ;; "';'.join(module_completion('''%s'''))\n"
-      ;; python-shell-completion-string-code
-      ;; "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
-      
       )
 
 
