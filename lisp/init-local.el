7f91518c919b9bf561c54e302745e2aa3131d72f
Justin Guenther <jguenther@gmail.com>
2015-10-09 15:17:49 -0600

    Add `zone` init

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1002,5 +1002,32 @@ supply a positive argument once more with C-u C-SPC."
 
 
 
+;; zone 'screensaver'
+(require 'zone)
+(setq zone-programs [
+                     zone-pgm-jitter
+                     zone-pgm-putz-with-case
+                     zone-pgm-dissolve
+                     zone-pgm-explode
+                     zone-pgm-whack-chars
+                     zone-pgm-rotate
+                     zone-pgm-rotate-LR-lockstep
+                     zone-pgm-rotate-RL-lockstep
+                     zone-pgm-rotate-LR-variable
+                     zone-pgm-rotate-RL-variable
+                     zone-pgm-drip
+                     zone-pgm-drip-fretfully
+                     zone-pgm-five-oclock-swan-dive
+                     zone-pgm-martini-swan-dive
+                     zone-pgm-rat-race
+                     zone-pgm-paragraph-spaz
+                     ;; zone-pgm-stress
+                     ;; zone-pgm-stress-destress
+                     zone-pgm-random-life
+                     ])
+(zone-when-idle 120)
+
+
+
 
 (provide 'init-local)
