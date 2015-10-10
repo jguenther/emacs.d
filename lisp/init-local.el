8c3555fa01661853acc6e909babc8ec287038158
Justin Guenther <jguenther@gmail.com>
2015-10-10 00:45:38 -0600

    zone config

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1006,16 +1006,16 @@ supply a positive argument once more with C-u C-SPC."
 ;; zone 'screensaver'
 (require 'zone)
 (setq zone-programs [
-                     zone-pgm-jitter
+                     ;;zone-pgm-jitter
                      zone-pgm-putz-with-case
                      zone-pgm-dissolve
                      zone-pgm-explode
                      zone-pgm-whack-chars
-                     zone-pgm-rotate
-                     zone-pgm-rotate-LR-lockstep
-                     zone-pgm-rotate-RL-lockstep
-                     zone-pgm-rotate-LR-variable
-                     zone-pgm-rotate-RL-variable
+                     ;;zone-pgm-rotate
+                     ;;zone-pgm-rotate-LR-lockstep
+                     ;;zone-pgm-rotate-RL-lockstep
+                     ;;zone-pgm-rotate-LR-variable
+                     ;;zone-pgm-rotate-RL-variable
                      zone-pgm-drip
                      zone-pgm-drip-fretfully
                      zone-pgm-five-oclock-swan-dive
