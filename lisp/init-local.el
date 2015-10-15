b4d38015129285cff6fca17f0300791c0cc108a7
Justin Guenther <jguenther@gmail.com>
2015-10-15 08:53:29 -0600

    Don't use perlysense in master

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -241,8 +241,7 @@ Moves point to the end of the inserted text. Does not change mark."
               ;;       '(ac-source-perl-completion))
               )))
 
-;; (after-load 'cperl-mode
-;;   (require 'init-perlysense))
+
 
 ;; arma script syntax is very similar to c
 (add-to-list 'auto-mode-alist '("\\.sq[fm]$" . c-mode))
