4bb02aa6f9c595eed1c85d5f54c6ba14e28f11bb
Justin Guenther <jguenther@gmail.com>
2015-10-18 23:00:29 -0600

    smooth-scrolling init

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -26,6 +26,12 @@
 
  scroll-restore-handle-cursor nil
 
+ redisplay-dont-pause t
+ scroll-margin 1
+ scroll-step 1
+ scroll-conservatively 10000
+ scroll-preserve-screen-position 1
+
  ;; shell-mode
  shell-completion-execonly nil
 
