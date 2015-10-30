13d175b1d6a2d93c1e73b9af461a5aca8190db36
Justin Guenther <jguenther@gmail.com>
2015-10-30 17:51:00 -0600

    Don't bind dash if not installed

--- lisp/init-dash.el
+++ lisp/init-dash.el
@@ -9,13 +9,14 @@
                (shell-command-to-string
                 (concat lsregister " -dump|grep com.kapeli.dash")))))))
 
-(when (and *is-a-mac* (not (package-installed-p 'dash-at-point)))
+(when (and *is-a-mac* (not (package-installed-p #'dash-at-point)))
   (message "Checking whether Dash is installed")
   (when (sanityinc/dash-installed-p)
     (require-package 'dash-at-point)))
 
-(when (package-installed-p 'dash-at-point)
-  (define-key mode-specific-map (kbd "D") 'dash-at-point)
-  (define-key mode-specific-map (kbd "E") 'dash-at-point-with-docset))
+(when (and (package-installed-p 'dash-at-point)
+           (sanityinc/dash-installed-p))
+  (define-key mode-specific-map (kbd "D") #'dash-at-point)
+  (define-key mode-specific-map (kbd "E") #'dash-at-point-with-docset))
 
 (provide 'init-dash)
