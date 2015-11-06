80a792ccd1ebe090aee3f078b33cabc7dc77307e
Justin Guenther <jguenther@gmail.com>
2015-11-06 16:21:41 -0600

    Don't need -i param to ipython

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -148,7 +148,7 @@ running."
 
 ;; ipython setup
 (setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i"
+      python-shell-interpreter-args ""
       python-shell-prompt-regexp "\\(In \\[[0-9]+\\]:\\|([Pp]db)\\|ipdb>\\|[(]+trepan2:?.+[)]+\\) "
       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
       )
