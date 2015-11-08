77164bf8ba338ebbbc1af671fb47decb09819cbb
Justin Guenther <jguenther@gmail.com>
2015-11-08 16:20:01 -0600

    use github-flavored markdown for README.md

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,11 +1,14 @@
 (require-package 'markdown-mode)
+(require-package 'gfm-mode)
 
 (after-load 'whitespace-cleanup-mode
-  (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
-
+  (push 'markdown-mode whitespace-cleanup-mode-ignore-modes)
+  (push 'gfm-mode whitespace-cleanup-mode-ignore-modes))
 
 (require-package 'mkdown)
 (after-load 'markdown-mode
   (setq-default markdown-css-paths mkdown-css-file-name))
+
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 (provide 'init-markdown)
