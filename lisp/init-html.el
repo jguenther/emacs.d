69d950bacb344e2781cc89b159cbaf39924d2014
Justin Guenther <jguenther@gmail.com>
2015-12-07 12:32:20 -0600

    Don't use linum-mode in html buffers

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,19 +1,13 @@
 (require-package 'tidy)
+(after-load 'html-mode
+  (add-hook 'html-mode-hook (lambda () (tidy-build-menu html-mode-map))))
 
 (require-package 'tagedit)
 (after-load 'sgml-mode
   (tagedit-add-paredit-like-keybindings)
-  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1)))
-  (add-hook #'sgml-mode-hook #'linum-mode)
-  )
+  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
 (add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
-
-(after-load 'html-mode
-  (add-hook #'html-mode-hook #'linum-mode)
-  (add-hook #'html-mode-hook (lambda () (tidy-build-menu html-mode-map)))
-  )
-
 
 ;; Note: ERB is configured in init-ruby-mode
 
